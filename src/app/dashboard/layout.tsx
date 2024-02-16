7c5fce0ffead85c4e7ce685833cf669c9618690d
rachidfrex <danzu2009@gmail.com>
2024-02-16 16:39:12 +0100

    Add @radix-ui/react-progress and lucide dependencies, and create Sidemenu component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import '../globals.css'
 import { ThemeProvider } from "@/components/ui/provider/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { Mode } from "@/components/ui/them-mode";
+import Sidemenu from '@/components/my-ui/sidemenu';
 
 export const metadata: Metadata = {
   title: "شركة القهوة التي توصلها",
@@ -19,9 +20,12 @@ export default function RootLayout({
       className="w-full min-h-screen grid grid-cols-5 relative place-content-start"
     >
       <div className="top-4 fixed z-50 right-4">
+      
+
         <Mode />
       </div>
       <aside className="w-full bg-primary-foreground z-50 h-screen col-span-1 border-r border sticky top-0 right-0">
+      <Sidemenu/>
       </aside>
       <div className="w-full relative overflow-auto col-span-4 ">
         <ThemeProvider
