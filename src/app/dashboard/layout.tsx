6515201ebf83f550b6b5cb003eddedd6c0f277ea
rachidfrex <2003043000250@ofppt-edu.ma>
2024-02-16 20:41:15 +0100

    Refactor dashboard layout and remove unused code

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,17 @@
 import type { Metadata } from "next";
 import '../globals.css'
-import { ThemeProvider } from "@/components/ui/provider/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
-import { Mode } from "@/components/ui/them-mode";
 import Sidemenu from '@/components/my-ui/sidemenu';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Menu } from "lucide-react";
+
 
 export const metadata: Metadata = {
   title: "شركة القهوة التي توصلها",
@@ -20,19 +28,32 @@ export default function RootLayout({
       className="w-full min-h-screen grid grid-cols-5 relative place-content-start"
     >
       <div className="top-4 fixed z-50 right-4">
-      
+        <Sheet>
+          <SheetTrigger>
+            <Menu />
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Are you absolutely sure?</SheetTitle>
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your account
+                and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
 
-        <Mode />
+
       </div>
-      <aside className="w-full bg-primary-foreground z-50 h-screen col-span-1 border-r border sticky top-0 right-0">
-      <Sidemenu/>
+      <aside className="w-full bg-primary-foreground z-50 h-screen hidden lg:block lg:col-span-1 border-r border sticky top-0 right-0">
+        <Sidemenu />
       </aside>
-      <div className="w-full relative overflow-auto col-span-4 ">
-        
-          {children}
-          <Toaster
-            position="top-right"
-          />
+      <div className="w-full relative overflow-auto col-span-5 lg:col-span-4 ">
+
+        {children}
+        <Toaster
+          position="top-right"
+        />
       </div>
     </div>
   );
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,6 @@
 import ApexChart from '@/components/my-ui/chart';
 // icon 
 import { Card } from '@/components/ui/card';
-import { Mode } from "@/components/ui/them-mode";
 import { Calendar } from 'lucide-react';
 import { BookCheck } from 'lucide-react';
 import { MessageCircle } from 'lucide-react';
@@ -13,9 +12,7 @@ import LineChart from '@/components/my-ui/chart-line';
 const Dashboard = () => {
   return (
     <main className="flex min-h-screen  flex-col items-center relative justify-center px-5">
-      <div className="top-4 fixed z-50 right-4">
-        <Mode />
-      </div>
+
       <div
         className='flex flex-col w-full relative z-0 px-4 py-3 overflow-auto gap-6 justify-start items-start'
       >
@@ -70,10 +67,10 @@ const Dashboard = () => {
           </div>
         </div>
         <div className="w-full grid grid-cols-3 gap-6">
-          <Card className="w-full col-span-2 h-96 p-2 shadow-none">
+          <Card className="w-full col-span-3  lg:col-span-2 h-96 p-2 shadow-none">
             <LineChart />
           </Card>
-          <Card className="w-full col-span-1 h-96 p-2 shadow-none"></Card>
+          <Card className="w-full col-span-3  lg:col-span-1 h-96 p-2 shadow-none"></Card>
         </div>
         <Card className="w-full grid grid-cols-3 h-96 shadow-none gap-6">
         </Card>
