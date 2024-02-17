908895375cad6a9fd73ee236b9432851aa5e6fb1
rachidfrex <2003043000250@ofppt-edu.ma>
2024-02-17 13:08:11 +0100

    Refactor RawProducts component and add table to display raw products

--- src/app/dashboard/raw-product/page.tsx
+++ src/app/dashboard/raw-product/page.tsx
@@ -1,17 +1,70 @@
-import { Mode } from "@/components/ui/them-mode";
 
-import React from 'react'
+import { getProduct, getProducts } from "@/(db)/product-pr";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useEffect, useState } from "react";
 
-const RawProducts = () => {
+
+
+const RawProducts = async () => {
+  const res = await getProducts();
+  console.log(res); 
+
+  
+
+
+  
   return (
-    
-    <div className="top-4 fixed z-50 right-4">
-      <Mode />
-      </div>
-      
-    
-    
-  )
-}
+    <div className="w-[80%] mt-20">
+      <Table>
+        <TableCaption>قائمة المنتجات الخام</TableCaption>
 
-export default RawProducts
+        <TableHeader>
+          <TableRow>
+            <TableCell className="w-[100px]">
+              <TableHead>المنتج</TableHead>
+            </TableCell>
+            <TableCell>
+              <TableHead>السعر</TableHead>
+            </TableCell>
+            <TableCell>
+              <TableHead>الكمية</TableHead>
+            </TableCell>
+            <TableCell className="text-left">
+              <TableHead>الإجمالي</TableHead>
+            </TableCell>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {res.data.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>{product.name}</TableCell>
+              <TableCell>{product.price}</TableCell>
+              <TableCell>{product.quantity}</TableCell>
+              
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={2}></TableCell>
+            <TableCell className="">الإجمالي الكلي</TableCell>
+            <TableCell className="text-left">
+
+            </TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </div>
+  );
+};
+
+export default RawProducts;
