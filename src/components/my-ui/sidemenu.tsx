bfb2ee284a7b9c7855692fea71cc3b1a4d5567b7
rachidfrex <2003043000250@ofppt-edu.ma>
2024-02-17 13:20:43 +0100

    Fix bug in login functionality

--- src/components/my-ui/sidemenu.tsx
+++ src/components/my-ui/sidemenu.tsx
@@ -13,25 +13,25 @@ function Sidemenu() {
         {/* الشعار  */}
         <div className=" uppercase flex gap-2 justify-center items-center text-xl  mt-5  font-semibold">
             <span> <Coffee /></span>
-            <h1> <Link href="/">نغمة</Link></h1>
+            <h1> <Link href="/dashboard">نغمة</Link></h1>
         </div>
         {/* نهاية الشعار  */}
         <hr className="w-[80%] flex mx-auto my-4 " />
         {/* عناصر القائمة */}
         <div className="flex flex-col gap-2  mr-5 text-md gap-y-5 mt-4 ">
-            <a href="/dashboard" className=" font-semibold flex items-center gap-2">
+            <Link href="/dashboard" className=" font-semibold flex items-center gap-2">
                 <BarChartBig />
                 لوحة القيادة
 
-            </a>
-            <a href="dashboard/raw-product" className=" font-semibold flex items-center gap-2">
+            </Link>
+            <Link href="dashboard/raw-product" className=" font-semibold flex items-center gap-2">
                 <Package  />
                 المنتج الاولي
-            </a>
-            <a href="#" className=" font-semibold flex items-center gap-2">
+            </Link>
+            <Link href="#" className=" font-semibold flex items-center gap-2">
                 <PackageOpen />
                  المنتج النهائي
-            </a>
+            </Link>
         </div>
         {/* نهاية عناصر القائمة */}
     </div>
