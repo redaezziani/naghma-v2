30de80799f393c56ea44fe5b09735058628eeedf
redaezziani <2003100500167@ofppt-edu.ma>
2024-02-19 12:24:53 +0000

    Add getVendurs function to retrieve vendors with balance

--- src/(db)/vendur.ts
+++ src/(db)/vendur.ts
@@ -18,3 +18,23 @@ export const createVendur = async (nom:string) => {
         await prisma.$disconnect();
     }
 }
+
+export const getVendurs = async () => {
+    try {
+        const vendurs = await prisma.vendur.findMany();
+        if (!vendurs || vendurs.length === 0) {
+            return { status: 'error', message: 'لم يتم العثور على البائعين' };
+        }
+        const vendursWithBalance = vendurs.map((vendur: any) => {
+            return {
+                ...vendur,
+                balance: vendur.le_prix_a_payer - vendur.le_prix_a_paye + vendur.frais_de_prix
+            }
+        });
+        return { status: 'success', message: 'تم العثور على البائعين بنجاح', data: vendursWithBalance };        
+    } catch (error: any) {
+        console.error(error);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
