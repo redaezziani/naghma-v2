fee18901c8798db469205053036a32d530330440
rachidfrex <2003043000250@ofppt-edu.ma>
2024-02-22 16:56:15 +0100

    Add total_sell_quantity to VendorInfo component

--- src/components/my-ui/vendor-Info.tsx
+++ src/components/my-ui/vendor-Info.tsx
@@ -1,18 +1,21 @@
 function VendorInfo(props: any) {
-  const { nom, le_prix_a_payer, le_prix_a_paye, frais_de_prix, balance } = props.vendur ?? {};
+  const { nom, le_prix_a_payer, le_prix_a_paye, frais_de_prix, balance, total_sell_quantity } = props.vendur ?? {};
   return (
     <div className='flex flex-col gap-4'>
       <h3 className='text-lg text-primary '>البيانات الشخصية</h3>
       <div className='flex font-semibold flex-col gap-3'>
-        <p>اسم البائع : <span className=' text-green-400'>{nom}</span></p>
-        <p>المبلغ الذي يجب أن يدفع : <span className=' text-slate-700'>{le_prix_a_paye}</span></p>
-        <p>المبلغ الذي تم إرجاعه : <span className=' text-green-400'>{le_prix_a_payer}</span></p>
-        <p>النفقات والمصاريف : <span className=' text-primary'>{frais_de_prix}</span></p>
+        <p>اسم البائع : <span className=' '>{nom}</span></p>
+        <p>المبلغ الذي يجب أن يدفع : <span className=' text-slate-700'>{le_prix_a_paye} د.م </span></p>
+        <p>المبلغ الذي تم إرجاعه : <span className=' text-green-400'>{le_prix_a_payer} د.م </span></p>
+        <p>النفقات والمصاريف : <span className=' text-primary'>{frais_de_prix} د.م </span></p>
         <p>
-          المبلغ المتبقي لدفعه:
+          المبلغ المتبقي لدفعه :
           <span className={
             ` ${balance <= 0 ? 'text-emerald-600' : 'text-destructive'} font-semibold`}
-          >{balance}</span></p>
+          >{balance} د.م </span></p>
+        <p>
+          إجمالي الكمية المأخوذة :
+          <span className=' text-primary'> {total_sell_quantity} كلغ</span></p>
       </div>
     </div>
   )
