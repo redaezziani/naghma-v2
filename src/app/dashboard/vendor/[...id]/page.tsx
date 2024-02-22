f25954d18a49b7d516febf827f4d0cca1e11e79f
redaezziani <2003100500167@ofppt-edu.ma>
2024-02-22 12:14:21 +0000

    Add VendorInfo and remove SelledProducts component from VendorPage

--- src/app/dashboard/vendor/[...id]/page.tsx
+++ src/app/dashboard/vendor/[...id]/page.tsx
@@ -1,17 +1,19 @@
 'use client';
-import { useEffect } from "react";
-import { getVendurById } from "@/(db)/vendur";
-import VendorInfo from "@/components/my-ui/vendor-Info";
-import SelledProducts from "@/components/my-ui/selled-products";
+import VendorInfo from '@/components/my-ui/vendor-Info';
+import SelledProducts from '@/components/my-ui/selled-products';
+import { useState, useEffect } from 'react';
+import { getVendurById } from '@/(db)/vendur';
 const VendorPage = ({ ...props }: any) => {
     let id = props.params.id[0]
+    const [data, setData] = useState<any>({})
     const handelData = async () => {
         try {
             const res = await getVendurById(id);
             if (res?.status === 'error') {
                 return;
             }
-            console.log(res);
+            console.log(res.data);
+            setData(res?.data ?? {});
         } catch (error) {
             console.log(error);
         }
@@ -27,19 +29,17 @@ const VendorPage = ({ ...props }: any) => {
      w-full
     lg:w-2/3
     px-6 py-3 relative">
-            
+
             <h1
                 className='text-2xl text-primary font-bold'
             >
                 ملف البائع
             </h1>
             <div>
-                <VendorInfo />
+                <VendorInfo vendur={data?.vendur??{}} />
+                
             </div>
-            <div className='w-full  '>
-                <SelledProducts />  
-            </div> 
-
+           
         </div>
     )
 }
--- src/components/my-ui/vendor-Info.tsx
+++ src/components/my-ui/vendor-Info.tsx
@@ -1,25 +1,20 @@
 import React from 'react'
 
-function VendorInfo() {
+function VendorInfo(props: any) {
+  const { nom, le_prix_a_payer, le_prix_a_paye, frais_de_prix, balance } = props.vendur??{};
   return (
     <div className='flex flex-col gap-4'>
-        
-        <h3 className='text-lg text-primary '>البيانات الشخصية</h3>
-        <div className='flex flex-col gap-3'>
-            <p>اسم البائع :</p>
-            <p>
-            المبلغ المطلوب ارجاعه : 
-            </p>
-            <p>
-            المبلغ الدي تم ارجاعه :
-            </p>
-            <p>
-            النفقات والمصاريف :
-            </p>
-        </div>
-        
+      <h3 className='text-lg text-primary '>البيانات الشخصية</h3>
+      <div className='flex font-semibold flex-col gap-3'>
+        <p>اسم البائع : <span className=' text-green-400'>{nom}</span></p>
+        <p>المبلغ الذي يجب أن يدفع : <span className=' text-green-400'>{le_prix_a_paye}</span></p>
+        <p>المبلغ الذي تم إرجاعه : <span className=' text-green-400'>{le_prix_a_payer}</span></p>
+        <p>النفقات والمصاريف : <span className=' text-green-400'>{frais_de_prix}</span></p>
+        <p>الرصيد : <span className=' text-red-400'>{balance}</span></p>
+      </div>
     </div>
   )
 }
+
 
 export default VendorInfo
