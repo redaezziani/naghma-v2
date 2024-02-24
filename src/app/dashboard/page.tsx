fc69097e50b5216f7393e8b25392bad89d37da7d
redaezziani <2003100500167@ofppt-edu.ma>
2024-02-24 13:27:10 +0000

    Remove unused imports and clean up code

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,6 @@
 'use client';
-import ApexChart from '@/components/my-ui/chart';
-// icon 
 import { Card } from '@/components/ui/card';
 import { Calendar } from 'lucide-react';
-import { BookCheck } from 'lucide-react';
-import { MessageCircle } from 'lucide-react';
-// shad cd 
-import { Progress } from "@/components/ui/progress"
-import { UserProfile } from '@/components/my-ui/user-profile';
 import LineChart from '@/components/my-ui/chart-line';
 import BarChart from '@/components/my-ui/chart-bar';
 import { useEffect, useState } from 'react';
@@ -16,7 +9,6 @@ const Dashboard = () => {
   const [earnings, setEarnings] = useState('')
   const getEarnings = async () => {
     const res = await getEarningsOfCurrentMonth();
-    
     //@ts-ignore
     setEarnings(res?.data ?? 0)
   }
@@ -30,12 +22,10 @@ const Dashboard = () => {
       <div
         className='flex flex-col w-full relative z-0 px-4 py-3 overflow-auto gap-6 justify-start items-start'
       >
-       
         <div className="flex w-full  mt-20 flex-col gap-3 justify-start items-start">
           <h1 className="text-2xl font-bold text-primary">
             الرئيسية
           </h1>
-
         </div>
         <div className="flex w-full flex-col gap-3 justify-start items-start">
 
@@ -57,32 +47,6 @@ const Dashboard = () => {
                 <p className="font-semibold text-xl mt-1">$215,000</p>
               </div>
               <Calendar className="w-9 h-9" />
-            </Card>
-
-            <Card className="w-full col-span-1 shadow-none py-7 px-5 flex justify-between items-center border rounded-lg">
-              <div>
-                <p className="text-xs">المهام</p>
-                <div className="flex flex-row w-full">
-                  <p className="font-semibold text-xl mt-1">50%</p>
-                  {/* <Progress value={33} /> */}
-                </div>
-
-              </div>
-              <Progress value={55} className="w-[66%] mt-6" />
-              <BookCheck className="w-9 h-9" />
-            </Card>
-            <Card className="w-full col-span-1  shadow-none py-7 px-5 flex justify-between items-center  rounded-lg">
-              <div>
-                <p className="text-sm text-primary">الطلبات المعلقة</p>
-                <p
-                className=' text-slate-400 text-xs mt-1 descreiption'
-                >
-                  جميع الطلبات التي لم يتم تسليمها بعد
-                </p>
-                <p className="font-semibold text-xl text-slate-800 mt-1">8</p>
-
-              </div>
-              <MessageCircle className="w-6 h-6 text-pretty" />
             </Card>
           </div>
         </div>
