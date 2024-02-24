c870d498d2057bac1115cf1a0bafab41297b90f3
redaezziani <2003100500167@ofppt-edu.ma>
2024-02-24 12:16:36 +0000

    Add logs relation to Produit_Final model

---
+++ prisma/migrations/20240224120507_fix_product/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `changes` on the `Produit_Final_logs` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Produit_Final_logs" DROP COLUMN "changes";
