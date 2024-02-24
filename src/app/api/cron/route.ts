68585836deb4717967def125f355c035ffe47d88
redaezziani <2003100500167@ofppt-edu.ma>
2024-02-24 15:36:55 +0000

    Update cron schedule and add node-cron dependency

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -3,15 +3,17 @@ import { NextResponse } from 'next/server';
 
 import { Resend } from 'resend';
 const resend = new Resend('re_E18ceeYD_MTkvyL3zqXs2ETpNipdssVh7');
-
+import cron from 'node-cron';
 
 export async function GET() {
+
     const response = await resend.emails.send({
         from: 'onboarding@resend.dev',
         to: 'klausdev2@gmail.com',
-        subject: 'Foreget Password Request',
+        subject: 'sample email from Resend',
         html:'<h1>Hello world</h1>',
         text: 'Welcome to the store!', 
     });
+    
     return NextResponse.json(response);
 }
