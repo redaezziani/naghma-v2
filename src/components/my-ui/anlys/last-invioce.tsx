9b64eefd5b5dcc60e9424c7c23d2d50b5ed34123
redaezziani <2003100500167@ofppt-edu.ma>
2024-02-26 17:15:17 +0000

    Refactor last-invioce.tsx component

--- src/components/my-ui/anlys/last-invioce.tsx
+++ src/components/my-ui/anlys/last-invioce.tsx
@@ -1,15 +1,5 @@
 "use client";
 import React, { forwardRef, HTMLAttributes } from 'react';
-/*
-  final = {data.finalResult}
-            finalStock = {data.finalResultStock}
-            total = {data.total}
-            totalExpenses = {data.totalExpenses}
-            totalFrais = {data.totalFrais}
-            initial_amount_price = {initial_amount_price}
-            
-*/
-// lets make a table for the data
 const ComponentToPrint = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
   (props, ref) => {
     //@ts-ignore
@@ -18,7 +8,59 @@ const ComponentToPrint = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElemen
     <div
      className='w-full flex flex-col justify-start items-start gap-4 p-6'
     ref={ref}>
-    
+     <div className="w-full flex justify-between items-center ">
+       <div className="flex flex-col gap-2 justify-start items-start">
+       <h2
+        className=' text-xl font-bold text-slate-700'
+        >
+          شركة نغمة للقهوة
+        </h2>
+        <p
+        className=' text-xs font-semibold  text-slate-600'
+        >
+          العنوان: 
+          <span
+          className=' text-slate-500 mr-2 font-medium'
+          >
+            المغرب - القنيطرة - بئر الرامي
+          </span>
+        </p>
+       </div>
+        <img 
+        className=' w-16  aspect-auto '
+        src="/naghma.png" alt="image" />
+      </div>
+      <h4
+      className=' mt-3 font-semibold text-slate-600'
+      >
+        الارباح الشهرية
+      </h4>
+      <table
+      className='w-full border-2 border-slate-200'
+      >
+        <thead
+        className='bg-slate-100 text-center'
+        >
+          <tr
+          className='text-xs font-semibold text-slate-700'
+          >
+            <th
+            className='p-2'
+            >
+              البداية
+            </th>
+          
+          </tr>
+        </thead>
+        <tbody
+        className='text-center'
+        >
+          <tr
+          className='text-xs font-semibold text-slate-700'
+          >
+          </tr>  
+        </tbody>
+      </table>
     </div>
     )
   }
