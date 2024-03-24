e5d53b658e8712d43e0d3b17dd10a6b5db6c1561
redaezziani <2003100500167@ofppt-edu.ma>
2024-03-24 22:10:14 +0000

    edit midlwaire

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,22 +7,7 @@ import { verifyToken } from './(db)/resnd/core';
 export const middleware= async (request: NextRequest) => {
     const token = request.cookies.has('token');
     const { pathname }: { pathname: string } = request.nextUrl;
-    if (!token &&  !['/signin', '/signup', '/forget-password'].includes(pathname)) {
-        return NextResponse.redirect(new URL('/signin', request.nextUrl.origin).toString());
-    }
-    const payload = await verifyToken();
-    if (token && !payload && !['/signin', '/signup', '/forget-password'].includes(pathname)) {
-        return NextResponse.redirect(new URL('/signin', request.nextUrl.origin).toString());
-    }
-    if (token &&  ['/signin', '/signup', '/forget-password'].includes(pathname)) {
-        return NextResponse.redirect(new URL('/dashboard', request.nextUrl.origin).toString());
-    }
-    if (token && payload && payload.role === 'user' && ['/dashboard/vendor', '/dashboard' , '/dashboard/vendor/add-vendor','/dashboard/vendor/vendors-logs' ,'/dashboard/vendor/payment', '/dashboard/vendor/retourn', '/dashboard/vendor/expenses'].includes(pathname)  || token && payload && payload.role === 'user' && pathname.startsWith('/dashboard/final-product')) {
-        return NextResponse.redirect(new URL(`/dashboard/vendor/${payload?.vendur_id}`, request.nextUrl.origin).toString());
-    }
-    if (token && payload && payload.role === 'admin' && ['/dashboard/vendor/add-vendor','/dashboard/vendor/vendors-logs' ,'/dashboard/vendor/payment', '/dashboard/vendor/retourn', '/dashboard/vendor/expenses'].includes(pathname)  || token && payload && payload.role === 'admin' && ['/dashboard/final-product/products-logs','/dashboard/final-product/add-final-product'].includes(pathname) || token && payload && payload.role === 'admin' && pathname.startsWith('/dashboard/final-product/update-product/')) {
-        return NextResponse.redirect(new URL(`/dashboard/`, request.nextUrl.origin).toString());
-    }
+ 
     return NextResponse.next();    
 }
 
